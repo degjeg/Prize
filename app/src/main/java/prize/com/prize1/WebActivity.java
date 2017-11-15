4ab24ae0e9e1b1d6f825ff132c3985987cf45a01
dagner <dengjun@meilele.com>
2017-11-15 11:02:13 +0800

    webview

--- app/src/main/java/prize/com/prize1/WebActivity.java
+++ app/src/main/java/prize/com/prize1/WebActivity.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ScrollView;
@@ -52,14 +53,14 @@ public class WebActivity extends Activity {
         String url = getIntent().getStringExtra("url");
         angle = getIntent().getIntExtra("angle", 0);
 
-//        findViewById(R.id.h_scrollView).setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View arg0, MotionEvent arg1) {
-//                return true;
-//            }
-//        });
+        // findViewById(R.id.h_scrollView).setOnTouchListener(new View.OnTouchListener() {
+        //     @Override
+        //     public boolean onTouch(View arg0, MotionEvent arg1) {
+        //         return true;
+        //     }
+        // });
 
-        webView.loadUrl(url);
+        // webView.loadUrl(url);
 
 
         webView.post(() -> {
@@ -100,31 +101,54 @@ public class WebActivity extends Activity {
 
             }
 
-
-
-
             webView.post(() -> {
-                        webView.loadUrl(url);
-                        webView.getSettings().setJavaScriptEnabled(true);
                         webView.setWebViewClient(new WebViewClient() {
                             @Override
-                            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                                 webView.loadUrl(url);
                                 return true;
                             }
                         });
                         webView.setWebChromeClient(new WebChromeClient());
+                        //声明WebSettings子类
+                        WebSettings webSettings = webView.getSettings();
 
+                        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+                        webSettings.setJavaScriptEnabled(true);
+                        // 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
+                        // 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+
+                        //支持插件
+                        // webSettings.setPluginsEnabled(true);
+
+                        //设置自适应屏幕，两者合用
+                        //webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+                        //webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+
+                        //缩放操作
+                        //webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+                        //webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+                        //webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+
+                        //其他细节操作
+                        //webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+                        //webSettings.setAllowFileAccess(true); //设置可以访问文件
+                        //webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+                        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+                        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+
+                        webView.getSettings().setJavaScriptEnabled(true);
                     }
             );
 
-//            webView.postDelayed(() -> {
-//                webView.loadUrl("javascript:document.getElementById('loginName').value='danger'");
-//            }, 3000);
-//
-//            webView.postDelayed(() -> {
-//                webView.loadUrl("javascript:document.getElementById('loginName').value='hahaha'");
-//            }, 6000);
+            webView.postDelayed(() -> {
+                webView.loadUrl(url);
+            }, 1);
+
+            //
+            // webView.postDelayed(() -> {
+            //     webView.loadUrl("javascript:document.getElementById('loginName').value='hahaha'");
+            // }, 6000);
 
         });
     }
